ae7ac5c3679714a41e2fa5bd7806e49f0ab63d94
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-07-01 17:44:52 +0300

    Handle exceptions in repo

--- lib/repo/weather_repo.dart
+++ lib/repo/weather_repo.dart
@@ -11,11 +11,12 @@ class WeatherRepo {
       'forecast?id=292223&appid=4343555b136db241cb08c95659de76cb';
 
   Future<Data<Forecast>> fetchForecast() async {
-    final response = await _apiService.request(
-        requestType: RequestType.GET, endPoint: _forecastEndPoint);
-    final json = jsonDecode(response);
-    return Data<Forecast>.success(Forecast.fromJson(json));
-    try {} catch (e) {
+    try {
+      final response = await _apiService.request(
+          requestType: RequestType.GET, endPoint: _forecastEndPoint);
+      final json = jsonDecode(response);
+      return Data<Forecast>.success(Forecast.fromJson(json));
+    } catch (e) {
       Data.handleException(e);
     }
   }
