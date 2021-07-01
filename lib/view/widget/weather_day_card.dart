88153c61fa677991ef579711cfdf4a18ea279165
Motasem Ghozlan <motasem.go.26@gmail.com>
2021-07-01 20:26:25 +0300

    Cancel navigation if the user has chosen the same date

--- lib/view/widget/weather_day_card.dart
+++ lib/view/widget/weather_day_card.dart
@@ -38,6 +38,10 @@ class WeatherDayCard extends StatelessWidget {
       ),
       onTap: () {
         final bloc = context.read<WeatherHomeBloc>();
+        // Cancel navigation if the user have chosen the same date
+        if (_dayForecast.dt == bloc.selectedDay.dt) {
+          return;
+        }
         // Tell bloc to use selected day data
         bloc.selectedDay = _dayForecast;
         // Pop any other opened route
